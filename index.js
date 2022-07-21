1c1063b68fe6f1563d24cc49f676f2f22a595416
Leo Volwahsen <volwahsenl@gmail.com>
2022-07-21 11:57:51 +0200

    h

---
+++ index.js
@@ -0,0 +1,38 @@
+//Teil A (leicht):
+//Schreibe eine Funktion mit dem namen getNextFibonacciNumber
+//Die Funktion erhält zwei Parameter
+//prevNum1 eine number
+//prevNum2 eine number
+//Die Funktion gibt eine number zurück
+//getNextFibonacciNumber gibt die nächste Fibonacci Zahl der Folge zurück.
+//Beispiel:
+//getNextFibonacciNumber(5,8) -> 13
+//getNextFibonacciNumber(0,1) -> 1
+//getNextFibonacciNumber(21,34) ->55
+
+function getNextFibonacciNumber(prevNum1, prevNum2) {
+    console.log(`---------Teil-A--------`);
+    let result = prevNum1 + prevNum2;
+    return result;
+}
+console.log(getNextFibonacciNumber(5, 8));
+
+/*
+Teil B (mittel):
+Schreibe eine Funktion mit dem namen getAllFibonacciNumbersBelow
+Die Funktion erhält einen Parameter
+num eine number
+Die Funktion gibt ein Array zurück
+getAllFibonacciNumbersBelow gibt ein Array aller Fibonacci Zahlen unter dem übergebenen Parameter num zurück
+Beispiel:
+getAllFibonacciNumbersBelow(10) -> [0, 1, 1, 2, 3, 5, 8]
+getAllFibonacciNumbersBelow(50) -> [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+getAllFibonacciNumbersBelow(144) -> [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+*/
+
+function getAllFibonacciNumbersBelow(num) {
+    console.log(`---------Teil-B--------`);
+    for (let i = 0; i < num; i - 1) {}
+    return Array();
+}
+console.log(getAllFibonacciNumbersBelow(10));
